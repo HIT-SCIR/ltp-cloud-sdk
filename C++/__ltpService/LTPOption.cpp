9b9abaf586d62823c18d4947915ddce0ad1ed393
chuj625 <chuj625@2cbbd092-f85e-11de-8591-91bc733d3be8>
2010-01-03 13:32:29 +0000

    LTPData ==> LTPOption
    git-svn-id: http://ltp-service.googlecode.com/svn/trunk@10 2cbbd092-f85e-11de-8591-91bc733d3be8

---
+++ C++/__ltpService/LTPOption.cpp
@@ -0,0 +1,40 @@
+#include "LTPOption.h"
+namespace HIT_IR_LTP{
+	const std::string LTP_Option::WS="ws";
+	const std::string LTP_Option::POS="pos";
+	const std::string LTP_Option::NE="ne";
+	const std::string LTP_Option::WSD="wsd";
+	const std::string LTP_Option::PARSER="parser";
+	const std::string LTP_Option::SRL="srl";
+	const std::string LTP_Option::ALL="all";
+
+	const std::string  LTP_Option::GB2312 = "gb2312";
+	const std::string  LTP_Option::GBK = "gbk";
+	const std::string  LTP_Option::UTF8 = "utf-8";
+
+	const char * const LTP_Option::TAG_DOC = "doc";
+	const char * const LTP_Option::TAG_NOTE = "note";
+	const char * const LTP_Option::TAG_PARA = "para";
+	const char * const LTP_Option::TAG_SENT = "sent";
+	const char * const LTP_Option::TAG_WORD = "word";
+	const char * const LTP_Option::TAG_CONT = "cont";
+	const char * const LTP_Option::TAG_POS = "pos";
+	const char * const LTP_Option::TAG_NE = "ne";
+	const char * const LTP_Option::TAG_PSR_PARENT = "parent";
+	const char * const LTP_Option::TAG_PSR_RELATE = "relate";
+	const char * const LTP_Option::TAG_WSD = "wsd";
+	const char * const LTP_Option::TAG_WSD_EXP = "wsdexp";
+	const char * const LTP_Option::TAG_SRL_ARG = "arg";
+	const char * const LTP_Option::TAG_SRL_TYPE = "type";
+	const char * const LTP_Option::TAG_BEGIN = "beg";
+	const char * const LTP_Option::TAG_END = "end";
+	const char * const LTP_Option::TAG_ID = "id";
+
+	const char * const LTP_Option::NOTE_SENT = "sent";
+	const char * const LTP_Option::NOTE_WORD = "word";
+	const char * const LTP_Option::NOTE_POS = "pos";
+	const char * const LTP_Option::NOTE_NE = "ne";
+	const char * const LTP_Option::NOTE_PARSER = "parser";
+	const char * const LTP_Option::NOTE_WSD = "wsd";
+	const char * const LTP_Option::NOTE_SRL = "srl";
+}
