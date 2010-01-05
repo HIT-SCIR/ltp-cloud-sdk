beff4c1dff8b04de0b3014ccafbfb9008678bbb7
chuj625 <chuj625@2cbbd092-f85e-11de-8591-91bc733d3be8>
2010-01-05 07:52:14 +0000

    version 15 add web client method this version add some examples
    git-svn-id: http://ltp-service.googlecode.com/svn/trunk@17 2cbbd092-f85e-11de-8591-91bc733d3be8

---
+++ JAVA/edu/hit/ir/examples/Ltp_example1.java
@@ -0,0 +1,46 @@
+package edu.hit.ir.examples;
+
+import java.util.ArrayList;
+import edu.hit.ir.ltpService.LTML;
+import edu.hit.ir.ltpService.LTPOption;
+import edu.hit.ir.ltpService.LTPService;
+import edu.hit.ir.ltpService.SRL;
+import edu.hit.ir.ltpService.Word;
+
+public class Ltp_example1 {
+	public static void main(String[] args) {
+
+		LTPService ls = new LTPService("zhhan@ir.hit.edu.cn"); 
+		
+		try {
+			LTML ltml = ls.analyze(LTPOption.ALL,"我们都是赛尔人。");
+//			ltml.printXml();
+
+			int sentNum = ltml.countSentence();
+			for(int i = 0; i< sentNum; ++i){
+//				逐句访问
+				ArrayList<Word> wordList = ltml.getWords(i);
+				System.out.println(ltml.getSentenceContent(i));
+				for(int j = 0; j < wordList.size(); ++j){
+					System.out.print("\t" + wordList.get(j).getWS());
+					System.out.print("\t" + wordList.get(j).getPOS());
+					System.out.print("\t" + wordList.get(j).getNE());
+					System.out.print("\t" + wordList.get(j).getWSD() + "\t" + wordList.get(j).getWSDExplanation());
+					System.out.print("\t" + wordList.get(j).getParserParent() + "\t" + wordList.get(j).getParserRelation());
+//					如果是谓词则输出
+					if(ltml.hasSRL() && wordList.get(j).isPredicate()){
+						ArrayList<SRL> srls = wordList.get(j).getSRLs();
+						System.out.println();
+						for(int k = 0; k <srls.size(); ++k){
+							System.out.println("\t\t" + srls.get(k).type + "\t" + srls.get(k).beg + "\t" + srls.get(k).end);
+						}
+					}
+					System.out.println();
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+}
