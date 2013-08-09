b11fa3abb33fc8a26416203b2d6aece5f79d2189
Oneplus <oneplus.lau@gmail.com>
2013-08-09 08:53:13 +0800

    add setup script for python

---
+++ Python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ltpservice',
+        version='1.0',
+        description='Python version of Language Technology Platform web service client',
+        author='Yijia Liu',
+        author_email='yjliu@ir.hit.edu.cn/',
+        url='http://ir.hit.edu.cn/ltp',
+        packages=['ltpservice', ],
+        )
